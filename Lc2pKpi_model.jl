25f69ed42f61f59d513e6ee9a8322cded7417b0d
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2024-04-04 23:58:38 +0200

    serialization Lc2ppiK

---
+++ Lc2pKpi_model.jl
@@ -0,0 +1,82 @@
+## This notebook take models from Lc2ppiKSemileptonicModelLHCb.jl
+# and serialize them into a dictionary that is saved to JSON file
+
+using Lc2ppiKSemileptonicModelLHCb
+using ThreeBodyDecaysIO
+using JSON
+
+model = published_model("Default amplitude model")
+
+model.chains[6].Xlineshape
+
+function lineshape_parser(Xlineshape::BreitWignerMinL)
+    appendix = Dict()
+    Xlineshape_str = Xlineshape.name * "_BW"
+    scattering, a = Xlineshape_str, Dict(
+        Xlineshape_str => Dict(
+            :type => "BreitWigner",
+            :mass => Xlineshape.pars.m,
+            :width => Xlineshape.pars.Γ,
+            :l => Xlineshape.l,
+            :ma => Xlineshape.m1,
+            :mb => Xlineshape.m2))
+    merge!(appendix, a)
+    FF_production = "BlattWeisskopf(resonance)"
+    FF_decay = "BlattWeisskopf(b-decay)"
+    # hard coded
+    dR, dΛc = 1.5, 5.0 # /GeV
+    a = Dict(
+        "BlattWeisskopf(resonance)" => Dict(
+            :type => "BlattWeisskopf",
+            :radius => dR
+        ),
+        "BlattWeisskopf(b-decay)" => Dict(
+            :type => "BlattWeisskopf",
+            :radius => dΛc
+        )
+    )
+    merge!(appendix, a)
+    (; scattering, FF_production, FF_decay), appendix
+end
+
+function lineshape_parser(Xlineshape::Flatte1405)
+    appendix = Dict()
+    Xlineshape_str = Xlineshape.name * "_Flatte"
+    scattering, a = Xlineshape_str, Dict(
+        Xlineshape_str => Dict(
+            :type => typeof(Xlineshape),
+            :mass => Xlineshape.pars.m,
+            :width => Xlineshape.pars.Γ))
+    merge!(appendix, a)
+    FF_production = ""
+    FF_decay = ""
+    (; scattering, FF_production, FF_decay), appendix
+end
+
+function lineshape_parser(Xlineshape::BuggBreitWignerMinL)
+    appendix = Dict()
+    Xlineshape_str = Xlineshape.name * "_BuggBW"
+    scattering, a = Xlineshape_str, Dict(
+        Xlineshape_str => Dict(
+            :type => typeof(Xlineshape),
+            :mass => Xlineshape.pars.m,
+            :width => Xlineshape.pars.Γ,
+            :gamma => Xlineshape.pars.γ))
+    merge!(appendix, a)
+    FF_production = ""
+    FF_decay = ""
+    (; scattering, FF_production, FF_decay), appendix
+end
+
+lineshape_parser(model.chains[6].Xlineshape) isa Tuple
+lineshape_parser(model.chains[1].Xlineshape) isa Tuple
+lineshape_parser(model.chains[end].Xlineshape) isa Tuple
+
+dict, appendix = serializeToDict(model, lineshape_parser)
+dict[:appendix] = appendix
+dict[:validation] = validation_section(
+    model, randomPoint(model.chains[1].tbs), dict[:reference_topology])
+# 
+open("Lc2ppiK.json", "w") do io
+    JSON.print(io, dict, 4)
+end
