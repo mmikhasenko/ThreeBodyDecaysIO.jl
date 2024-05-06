35aaf44e3485173a636c6b28abdac1273532db04
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2024-05-06 23:40:03 +0200

    validation to tests

--- test/model_content.jl
+++ test/model_content.jl
@@ -6,6 +6,43 @@ using ThreeBodyDecaysIO.DataFrames
 using Test
 
 
+list_of_models = readdir(joinpath(@__DIR__, "..", "models"))
+
+map(list_of_models) do file_name
+    @info "⭐ Reading model from $file_name ⭐"
+    # @testset "Test of $file_name" begin
+    @testset "Test of $file_name" begin
+        # get the JSON content
+        input = open(joinpath(@__DIR__, "..", "models", file_name)) do io
+            JSON.parse(io)
+        end
+        model_descrition = first(input["distributions"])
+        @test model_descrition["type"] == "HadronicUnpolarizedIntensity"
+
+        @test haskey(model_descrition, "decay_description")
+        @unpack decay_description = model_descrition
+
+        @test haskey(decay_description, "reference_topology")
+        @unpack reference_topology = decay_description
+
+        @test flatten_topology(reference_topology) |> sort == [1, 2, 3]
+
+        @info "🐰 Parcing kinamatics 🐰"
+        @unpack kinematics = decay_description
+        tbs = dict2instance(ThreeBodySystem, kinematics)
+
+        @test haskey(input, "functions")
+    end
+end
+
+
+
+# More serious stuff
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
 # extra likeshapes for testing
 @with_kw struct BreitWignerWidthExpLikeBugg <: HadronicLineshapes.AbstractFlexFunc
     m::Float64
@@ -26,52 +63,58 @@ function ThreeBodyDecaysIO.dict2instance(::Type{BreitWignerWidthExpLikeBugg}, di
 end
 
 
-list_of_models = readdir(joinpath(@__DIR__, "..", "models"))
-
 map(list_of_models) do file_name
     @info "⭐ Reading model from $file_name ⭐"
     # @testset "Test of $file_name" begin
-    let
-        @show "Test of $file_name"
+    @testset "Test of $file_name" begin
         # get the JSON content
         input = open(joinpath(@__DIR__, "..", "models", file_name)) do io
             JSON.parse(io)
         end
-        model_descrition = first(input["distributions"])
-        @test model_descrition["type"] == "HadronicUnpolarizedIntensity"
-
-        @test haskey(model_descrition, "decay_description")
-        @unpack decay_description = model_descrition
-
-        @test haskey(decay_description, "reference_topology")
-        @unpack reference_topology = decay_description
-
-        @test flatten_topology(reference_topology) |> sort == [1, 2, 3]
-
-        @info "🐰 Parcing kinamatics 🐰"
-        @unpack kinematics = decay_description
-        tbs = dict2instance(ThreeBodySystem, kinematics)
-
-
-        @test haskey(input, "functions")
-        @unpack functions = input
 
         @info "🔥 Building lineshapes functions 🔥"
         workspace = Dict{String,Any}()
         for fn in functions
             @unpack name, type = fn
+            @info "   🎈$name"
             instance_type = eval(Symbol(type))
             workspace[name] = dict2instance(instance_type, fn)
         end
         # @test dict2instance(ThreeBodyDecay, decay_description; workspace) isa ThreeBodyDecay
 
-        @info "🦊 Establishing decay chains 🦊"
-        df = dict2instance.(DecayChain, decay_description["chains"]; tbs, workspace) |> DataFrame
-        model = ThreeBodyDecay(Vector{Pair{String,Tuple{Complex,AbstractDecayChain}}}(df.name .=> zip(df.coupling, df.chain)))
+        @info "🦊 Building distributions functions 🦊"
+        # build distributions from JSON array,
+        @unpack distributions = input
+        for dist in distributions
+            @unpack name, type = dist
+            @info "   🎈$name"
+            instance_type = eval(Symbol(type))
+            workspace[name] = dict2instance(instance_type, distributions[1]; workspace)
+        end
 
-        @test model isa ThreeBodyDecay
+        @info "👡 Performing validation 👡"
+        @unpack misc, parameter_points = input
+        @unpack amplitude_model_checksums = misc
+
+        @testset "Validation" begin
+            map(amplitude_model_checksums) do check_point_info
+                @unpack name, value, distribution = check_point_info
+                # 
+                # pull distribution
+                dist = workspace[distribution]
+
+                # pull correct parameter point
+                parameter_points_dict = array2dict(parameter_points; key="name")
+                # find the point in the list of points
+                parameter_point = parameter_points_dict[name]
+                # compute, compare
+                _parameters = array2dict(parameter_point["parameters"];
+                    key="name", apply=v -> v["value"])
+                @test value ≈ dist(_parameters)
+                return "🟢"
+            end
+        end
     end
 end
-
 
 
